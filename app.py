91be9e96e0ed5a48e74682f82c8eb4187792d5a0
shohelshaik <shohel2654@gmail.com>
2025-12-17 12:05:56 +0530

    Fix file path syntax in app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.set_page_config(
     page_title="student Task Dashboard",
     layout="wide"
 )
-html_file = path("FDS,.html").read_text(encoding="utf-8")
+html_file = path("FDS.html").read_text(encoding="utf-8")
 st.components.v1.html(html_file,height=900,scrolling=True)
+
 
